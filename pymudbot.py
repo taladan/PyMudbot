505b7940fa3a5107025a4d0af3daee951d21bb0a
D.J. Crosby <taladan@gmail.com>
2022-02-10 20:14:36 -0500

    Incremental push for code review

--- pymudbot.py
+++ pymudbot.py
@@ -2,21 +2,55 @@
 
 import asyncio
 import pyinputplus as pyip
+import os
+import sys
+import mudtelnet as mt
 
 
 class SessionHandler:
+    def __init__(self):
+        self.DEBUG = False
+
     async def connect(self, host: str, port: int):
+        """
+        handle connections
+        """
         self.reader, self.writer = await asyncio.open_connection(host, port)
 
         while True:
             msg = await self.reader.readline()
             if msg is None:
-                asyncio.sleep(1)
+                asyncio.sleep(0.25)
                 continue
-            self.handle_msg(msg)
+            else:
+                response = await self.handle_msg(msg)
 
-    def handle_msg(self, msg):
-        print(msg)
+    async def log(self, msg):
+        # Simple log to file for now
+        with open("mudbot.log", "ab") as logfile:
+            logfile.write(msg)
+        if self.DEBUG:
+            print(f"Writing output to {os.curdir}{logfile.name}")
+
+    def telnet_handler(self, msg):
+        # This doesn't work
+        tn_conn = mt.TelnetConnection()
+        out_buffer = bytearray()
+        out_events = list()
+        frame, size = mt.TelnetFrame.parse(msg)
+        processed_frame = tn_conn.process_frame(frame, out_buffer, out_events)
+        if processed_frame[0]:
+            print("foo")
+
+    async def handle_msg(self, msg):
+        """
+        This will handle incoming messages, negotiate telnet options, etc.
+        """
+        reply = self.telnet_handler(msg)
+        self.logTask = asyncio.create_task(self.log(msg))
+        await self.logTask
+        response = None
+        return response or reply
 
     async def request(self, msg):
         self.writer.write(msg)
@@ -24,6 +58,7 @@ class SessionHandler:
 
 
 if __name__ == "__main__":
+
     # Get host & port
     hostname = pyip.inputURL("What host are we connecting to? ")
     pt = pyip.inputInt("What port? ")
